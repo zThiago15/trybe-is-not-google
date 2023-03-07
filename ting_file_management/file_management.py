5bd766c11032b58f2bd968376d957d8ec2037dc7
Thiago Dias <thiaguinhodias.15@gmail.com>
2023-03-07 20:39:16 +0000

    feat: import text file, validate it and return news

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,19 @@
+"""module sys to provide the file object 'sys.stderr' when there's an error
+and os to manipulate path"""
+import sys
+import os
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    """Validate and read a txt file"""
+    _, ext = os.path.splitext(path_file)
+    if ext != '.txt':
+        print('Formato inválido', file=sys.stderr)
+        return []
+
+    if not os.path.exists(path_file):
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return []
+
+    with open(path_file, "r", encoding="utf-8") as file:
+        return file.read().split('\n')
+        
