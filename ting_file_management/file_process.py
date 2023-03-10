022d98d651bd6ae2aa44518809cb9157979dab11
Thiago Dias <thiaguinhodias.15@gmail.com>
2023-03-10 16:23:24 +0000

    feat: req3 - transform list from txt_importer into dict

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,9 +1,22 @@
-# from ting_file_management.file_management import txt_importer
+from ting_file_management.file_management import txt_importer
 from ting_file_management.queue import Queue
 
 
 def process(path_file: str, instance: Queue):
     """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        file = instance.search(index)
+        if file["nome_do_arquivo"] == path_file:
+            return False
+
+    content = txt_importer(path_file)
+
+    file_info = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(content),
+        "linhas_do_arquivo": content,
+    }
+    print(file_info)
 
 
 def remove(instance):
